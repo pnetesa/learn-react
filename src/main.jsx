26c6ab9238b8f384771ad0ee2c39d581a2a41fcc
Pavel Netesa <pavel.netesa@dataart.com>
2016-06-13 17:19:44 +0300

    Example 2 - First React App

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,5 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
+var ListManager = require('./components/ListManager.jsx');
 
-var List = require('./components/List.jsx');
-
-ReactDOM.render(<List />, document.getElementById('ingredients'));
+ReactDOM.render(<ListManager title="Ingredients" />, document.getElementById('ingredients'));
